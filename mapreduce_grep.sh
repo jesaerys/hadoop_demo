539d398f8138c1b5d7e6de17098be37f9d5e5726
jesaerys <jacob.simones@gmail.com>
2014-12-13 23:02:05 -0600

    Created mapreduce_grep.sh

---
+++ mapreduce_grep.sh
@@ -0,0 +1,82 @@
+#! /usr/bin/env bash
+
+
+# Use Hadoop/MapReduce to grep for all words containing the substring "am" in
+# the first paragraph of Lorem Ipsum.
+#
+# The actual MapReduce grepping task is performed by premade Java code (a jar
+# file) from the example library packaged with Hadoop. Adapted from
+# http://hadoop.apache.org/docs/current/hadoop-project-dist/hadoop-common/SingleCluster.html.
+#
+# Output files are placed in $HOME/hadoop_tmp/output. The contents of the main
+# output file (part-r-00000) should be,
+#
+#   6	amet
+#   2	quam
+#   1	ullamcorper
+#   1	ullam
+#   1	diam
+
+
+HADOOP_DIR=/usr/local/Cellar/hadoop/2.6.0
+
+
+# Create a Hadoop distributed file system (HDFS) at the location indicated by
+# the hadoop.tmp.dir parameter in core-site.xml. First make sure that no
+# cluster processes are currently running, then clear all contents of the HDFS
+# directory if it already exists and create it if it doesn't.
+$HADOOP_DIR/sbin/stop-dfs.sh
+HDFS_DIR="$(hdfs getconf -confKey hadoop.tmp.dir)"
+if [[ -e $HDFS_DIR ]]; then
+    rm -rf $HDFS_DIR
+fi
+mkdir -p $HDFS
+hdfs namenode -format
+
+
+# Start the single-node cluster. This script will start the SecondaryNameNode,
+# NameNode, and DataNode processes (use the jps command to check). HDFS
+# operations only work when the cluster is running.
+$HADOOP_DIR/sbin/start-dfs.sh
+
+
+# Designate /data as the location for the input data on the HDFS; create it
+hdfs dfs -mkdir -p /data
+
+# Note: The HDFS is its own file system, so directories created within it have
+# no corresponding entity in the local file system. The contents of the HDFS
+# are shown using the special ``hdfs dfs -ls`` command rather than with the
+# standard ``cd`` and ``ls`` commands. For example, ``hdfs dfs -ls -R /`` will
+# return a recursive listing for the whole HDFS.
+#
+# Note: MapReduce will automatically create /user and /user/<username> on the
+# HDFS. Both directories can be ignored for this example.
+
+
+# Empty out the data directory and copy fresh data into it
+hdfs dfs -rm -f -r /data/*
+hdfs dfs -put lorem_ipsum.txt /data/
+
+
+# Grep the input data for the given pattern. Designate /results as the
+# (automatically created) location for the output.
+EXAMPLE_DIR=$HADOOP_DIR/libexec/share/hadoop/mapreduce
+EXAMPLE_NAME=hadoop-mapreduce-examples-2.6.0.jar
+EXAMPLE_PATH=$EXAMPLE_DIR/$EXAMPLE_NAME
+INPUT_DIR=/data
+OUTPUT_DIR=/results
+PATTERN='[a-z]*am[a-z]*'
+hadoop jar $EXAMPLE_PATH grep $INPUT_DIR $OUTPUT_DIR $PATTERN
+
+
+# Copy the results to the local file system for safe keeping
+LOCAL_OUTPUT_DIR=$HOME/hadoop_tmp/output
+if [[ -e $LOCAL_OUTPUT_DIR ]]; then
+    rm -rf $LOCAL_OUTPUT_DIR
+fi
+mkdir -p $LOCAL_OUTPUT_DIR
+hdfs dfs -get $OUTPUT_DIR/* $LOCAL_OUTPUT_DIR
+
+
+# Stop the single-node cluster
+$HADOOP_DIR/sbin/stop-dfs.sh
